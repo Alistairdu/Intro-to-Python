a6bd570dba3c45079b82a4cc34d32ecc4849bab2
Alistairdu <alistair.dupont@gmail.com>
2020-11-25 10:44:02 -0500

    Added notes on PDFs and CDFs and SciPy  python access

--- Code.py
+++ Code.py
@@ -1,6 +1,12 @@
+# Invent problems for all kinds of stats we have available and solve them
 from math import e, factorial
 from scipy.stats import poisson
 
+def choose(n,k):
+    return factorial(n)/(factorial(n-k) * factorial(k))
+
+def binomial(n,k,p):
+    return choose(n,k) * p**k * (1-p)**(n-k)
 
 def geo(k,p): # first success is on kth trial
     return (1-p)**(k-1) * p
@@ -11,7 +17,7 @@ def geo_cdf(k,p):
 def geo_cdff(k,p):
     return 1-(1-p)**k
 
-def poisson2(lamb,k):
+def poisson(lamb,k):
     return lamb**k * e**-lamb / factorial(k)
 
 def poisson_cdf(lamb,k):
@@ -20,12 +26,31 @@ def poisson_cdf(lamb,k):
 def poisson_mv(lam):
     return lam, lam
 
-lamb = 20
-k = 25
-# p = 0.00016
+lamb = 5
+n = 10
+k = 5
+p = 0.5
 
-print(poisson.pmf(k=25,mu=20))
+# First, import the necessary function from the scipy.stats package
+from scipy.stats import norm
+
+# Call the norm function to make the calculation
+print(norm.cdf(53, loc=46, scale=1.75))
+print(norm.cdf(3.54285714286))
+
+
+# print(binomial(n,k,p))
+# print(poisson(lamb,k))
+
 
 # def geo_mv(p):
 #     return 1/p, (1-p)/p**2
+'''
+you are riding you bike downtown. In any 10 blocks, 
+you realize that you have on average 2 blocks w police cars.
+What is the probability that you see 7 blocks w police cars in the next 10 blocks?
 
+
+
+
+'''
